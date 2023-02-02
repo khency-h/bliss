ca2ce14231c45680d6da551d4918d60a115db06b
khency_h <khency.hiciano@gmail.com>
2023-02-02 14:39:39 -0500

    wip: Creates external controller file for routes

---
+++ controllers/movies.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const router = express.Router();
+
+// INDUCES
+
+// Index
+
+// New
+
+// Delete
+
+// Update
+
+// Create
+
+// Edit
+
+// Show
+
+module.exports = router;
