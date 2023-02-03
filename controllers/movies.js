2fd6e8c39bfe82c744fee617b5b38bb7c6481de6
khency_h <khency.hiciano@gmail.com>
2023-02-03 13:46:08 -0500

    wip: Defines new and create route

--- controllers/movies.js
+++ controllers/movies.js
@@ -1,17 +1,26 @@
 const express = require('express');
 const router = express.Router();
+const Movie = require('../models/movie');
 
 // INDUCES
 
 // Index
 
 // New
+router.get('/movies/new', (req, res) => {
+    res.render('new.ejs');
+});
 
 // Delete
 
 // Update
 
 // Create
+router.post('/movies', (req, res) => {
+    Movie.create(req.body, (err, createdMovie) => {
+        res.send(createdMovie);
+    });
+});
 
 // Edit
 
