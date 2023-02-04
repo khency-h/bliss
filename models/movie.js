95e0082d1c267ed4cfc250ef6eabf5d2448ab5ee
khency_h <khency.hiciano@gmail.com>
2023-02-03 22:56:31 -0500

    wip: Updates seed data and schema

--- models/movie.js
+++ models/movie.js
@@ -4,7 +4,9 @@ const Schema = mongoose.Schema;
 const movieSchema = new Schema({
     title: { type: String, required: true },
     movieDescription: { type: String, required: true },
-    movieImg: { type: String }
+    movieImg: { type: String },
+    price: { type: Number },
+    qty: { type: Number }
 }, { timestamps: true });
 
 module.exports = mongoose.model('Movie', movieSchema);
